e2154b9c327e2d9fca8990ddc382acd79db43194
Elga Ridlo <elgaridlo@Elgas-MacBook-Air.local>
2023-05-01 19:59:50 +0700

    Deploy React app to GitHub Pages ver 2

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-const urls = new Map()
-
-urls.set('localhost', 'http://localhost:9000')
-// urls.set(
-//   'react-job-udemy.herokuapp.com/',
-//   'https://fastify-udemy.herokuapp.com',
-// )
-
-export const baseUrl = urls.get(window.location.hostname)
